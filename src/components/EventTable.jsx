92ea1ac58c595055102d4bc62edbf20ec8f2d4f1
bseifert14 <ben.seifert14@gmail.com>
2025-07-07 09:13:00 -0400

    Cleanup formatting to better fit mobile

--- src/components/EventTable.jsx
+++ src/components/EventTable.jsx
@@ -6,61 +6,63 @@ import { getIcon } from "../utils/getIcon";
 
 export default function EventTable({ events = [], onSelectEvent }) {
   return (
-    <table className={styles.table}>
-      <thead className={styles.thead}>
-        <tr>
-          <th className={styles.thIcon} />
-          <th className={styles.th} />
-          <th className={styles.th}>Event</th>
-          <th className={styles.th}>Location & Time</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {events.map((event) => (
-          <tr key={event.id} className={styles.row}>
-            <td className={styles.tdIcon}>
-              {React.createElement(getIcon(event.event_type), { size: 18, strokeWidth: 1.5 })}
-            </td>
-            <td className={styles.imageCell}>
-              <img
-                src={event.event_photo_url || getDefaultImage(event.event_type)}
-                alt={event.event_name}
-                className={styles.eventImage}
-              />
-            </td>
-            <td className={styles.td}>
-              <div className={styles.eventName}>
-                {event.event_name}
-              </div>
-            </td>
-            <td className={styles.td}>
-              <div className={styles.locationTime}>
-                <span>{event.event_location}</span>
-                <br />
-                <span>{formatDate(event.event_date)}</span>
-              </div>
-            </td>
-            <td className={styles.actionCell}>
-              <div className={styles.actions}>
-                <button
-                  className={styles.infoButton}
-                  onClick={() => onSelectEvent?.(event)}
-                >
-                  More Info
-                </button>
-                <button
-                  className={styles.dotsButton}
-                  onClick={() => onSelectEvent?.(event)}
-                >
-                  <SquareArrowUpRight size={18} strokeWidth={1.5} />
-                </button>
-              </div>
-            </td>
+    <div className={styles.tableWrapper}>
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr>
+            <th className={styles.thIcon} />
+            <th className={styles.th} />
+            <th className={styles.th}>Event</th>
+            <th className={styles.th}>Location & Time</th>
+            <th className={styles.th} />
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {events.map((event) => (
+            <tr key={event.id} className={styles.row}>
+              <td className={styles.tdIcon}>
+                {React.createElement(getIcon(event.event_type), { size: 18, strokeWidth: 1.5 })}
+              </td>
+              <td className={styles.imageCell}>
+                <img
+                  src={event.event_photo_url || getDefaultImage(event.event_type)}
+                  alt={event.event_name}
+                  className={styles.eventImage}
+                />
+              </td>
+              <td className={styles.td}>
+                <div className={styles.eventName}>
+                  {event.event_name}
+                </div>
+              </td>
+              <td className={styles.td}>
+                <div className={styles.locationTime}>
+                  <span>{event.event_location}</span>
+                  <br />
+                  <span>{formatDate(event.event_date)}</span>
+                </div>
+              </td>
+              <td className={styles.actionCell}>
+                <div className={styles.actions}>
+                  <button
+                    className={styles.infoButton}
+                    onClick={() => onSelectEvent?.(event)}
+                  >
+                    More Info
+                  </button>
+                  <button
+                    className={styles.dotsButton}
+                    onClick={() => onSelectEvent?.(event)}
+                  >
+                    <SquareArrowUpRight size={18} strokeWidth={1.5} />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
