45100d71d73f1e79950917da7bea40ae7feae404
bseifert14 <ben.seifert14@gmail.com>
2026-06-12 16:18:00 -0400

    Footer renders differently in production. Testing out updating the elements

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -4,17 +4,19 @@ import FooterLogo from './FooterLogo';
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <FooterLogo />
+    <footer>
+      <div className={styles.footer}>
+        <FooterLogo />
 
-      <div className={styles.footerLinks}>
-        <a href="#">Privacy</a>
-        <a href="#">Terms</a>
-        <Link to="/contact">Contact</Link>
-      </div>
+        <div className={styles.footerLinks}>
+          <a href="#">Privacy</a>
+          <a href="#">Terms</a>
+          <Link to="/contact">Contact</Link>
+        </div>
 
-      <div className={styles.footerCopy}>
-        © 2026 On Tap Stowe, All rights reserved.
+        <div className={styles.footerCopy}>
+          © 2026 On Tap Stowe, All rights reserved.
+        </div>
       </div>
     </footer>
   );
